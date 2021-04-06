1a94b37d2133d184f636ba14b899172f37f9cab7
maarten-biolib <58252868+maarten-biolib@users.noreply.github.com>
2021-04-06 11:36:33 +0200

    bugfix

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 # If one of the preset modes is chosen, run this with the other input arguments
-if echo "linsi ginsi einsi fftnsi fftns nwns nwnsi" | grep -w $0 > /dev/null; then
+if echo "linsi ginsi einsi fftnsi fftns nwns nwnsi" | grep -w "$0" > /dev/null; then
     $@
 else
     mafft $@
