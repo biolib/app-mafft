beeaef24ccd03860a01b18199dc8306f94511081
maarten-biolib <58252868+maarten-biolib@users.noreply.github.com>
2021-04-06 11:42:06 +0200

    presets dont work

--- app.sh
+++ app.sh
@@ -1,6 +1,1 @@
-# If one of the preset modes is chosen, run this with the other input arguments
-if echo "linsi ginsi einsi fftnsi fftns nwns nwnsi" | grep -w "$0" > /dev/null; then
-    $@
-else
-    mafft $@
-fi
+mafft $@
